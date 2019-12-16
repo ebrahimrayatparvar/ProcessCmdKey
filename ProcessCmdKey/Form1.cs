874c1c354847671bb8130d726cabdbd9569be7ff
Ebrahimrayatparvar <ebrahim.rayatparvar@hotmail.com>
2019-12-17 00:59:12 +0330

    init

--- ProcessCmdKey/Form1.cs
+++ ProcessCmdKey/Form1.cs
@@ -16,5 +16,33 @@ namespace ProcessCmdKey
         {
             InitializeComponent();
         }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.W:
+                    button2.Location = new Point(button2.Location.X, button2.Location.Y - 5);
+                    break;
+                case Keys.S:
+                    button2.Location = new Point(button2.Location.X, button2.Location.Y + 5);
+                    break;
+                case Keys.Up:
+                    button1.Location = new Point(button1.Location.X, button1.Location.Y - 5);
+                    break;
+                case Keys.Down:
+                    button1.Location = new Point(button1.Location.X, button1.Location.Y + 5);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                MessageBox.Show("Test");
+            }
+        }
     }
 }
